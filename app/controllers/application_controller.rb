76f3993e47f4b862281b6c040b3c21c24cd0a276
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-04-29 16:55:59 +0900

    コメント箇所の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
-  Bundler.require(platform)
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
+Bundler.require(platform)
 
 module ChatSpace
   class Application < Rails::Application

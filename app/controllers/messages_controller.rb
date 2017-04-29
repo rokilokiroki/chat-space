e6573731be1629a7437897395e21ba2b4c469856
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-04-29 21:34:11 +0900

    made sign up

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :move_to_sign_up
+  def index
+
+  end
+
+
+  private
+  def move_to_sign_up
+    redirect_to new_user_registration_path unless user_signed_in?
+  end
 end

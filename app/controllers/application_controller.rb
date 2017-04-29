ad02fd6f041d7a5d2e8cbf73ba43134951ca529d
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-04-29 16:42:08 +0900

    コメント箇所の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
+  Bundler.require(platform)
   before_action :configure_permitted_parameters, if: :devise_controller?
+  private
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end

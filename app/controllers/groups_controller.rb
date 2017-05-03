9236ebaade0a73ffb4587a8de2a9ca83952aae22
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-05-03 19:54:10 +0900

    コメント箇所の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,17 +2,21 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
   def create
-    Group.create(post_parms)
+    group = Group.new(post_parms)
+    group.save
     redirect_to controller: :messages, action: :index
   end
 
   def edit
-    
+
   end
+
   private
+
   def post_parms
     params.require(:group).permit(:name, {:user_ids => []})
   end
+
 end
-  

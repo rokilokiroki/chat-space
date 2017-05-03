4c1f75bd21e9421b3addc7b9eaee3a2391645a42
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-05-03 18:13:19 +0900

    making group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,18 @@
 class GroupsController < ApplicationController
   def new
-    
+    @group = Group.new
+  end
+  def create
+    Group.create(post_parms)
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
     
   end
+  private
+  def post_parms
+    params.require(:group).permit(:name, {:user_ids => []})
+  end
 end
+  
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :user_groups
+  accepts_nested_attributes_for :users
   has_many :messages
   has_many :user_groups
 end

122091eca503517cd898102cec09ac621656137e
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-05-06 18:46:19 +0900

    コメント箇所の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  before_action :render1, only: :update
   def index
   end
 
@@ -15,18 +16,33 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    @user_names = User.where.not(name: current_user.name)
+    #@をつける、インスタンス変数にすることは可視化に便利？
   end
 
   def update
+    group = Group.find(params[:id])
+      # / form_forタグに最初からupdateパスは埋め込まれている。すでにカラムが存在しているインスタンスならupdateアクションに自動的に振り分けられる。つまりパスを指定していたpictweetの<%= form_tag("/tweets/#{@tweet.id}", method: :patch ) do %>はform_forにおいてはいらないということになる
+      if group.update(post_parms)
+        redirect_to "/groups/#{group.id}/messages",notice: 'グループを作成しました'
+      else
+        flash.now[:notice]="グループの作成にシッパイ"
+        render :edit
+      end
+  end
+
+  def render1
     @group = Group.find(params[:id])
-    @group.update(post_parms)
-    redirect_to controller: :groups, action: :index
+    @user_names = User.where.not(name: current_user.name)
   end
 
   private
 
   def post_parms
-    params.require(:group).permit(:name, {:user_ids => []})
+    user_ids = params[:group]["user_ids"]
+    user_ids << current_user.id.to_s
+    # コレクションボックスのidは文字列だからto_s
+    params.require(:group).permit(:name).merge(user_ids: user_ids)
   end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :messages
+  validates :name, presence: true
 end

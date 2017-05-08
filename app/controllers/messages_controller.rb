3cc66db1f3c0fd25c186a3e9364e762de8ff2a04
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-05-08 12:47:10 +0900

    message_post

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,23 @@ class MessagesController < ApplicationController
     #このgroup_idは/groups/:group_id/messages(.:format)のgroup_idである。参照rake routes
     #group_idをとってくることによってそのグループの情報を取ってくることが出来る。そしてその情報からeditに飛べる。editに跳ぶにはここのindexの情報が必須だった。
     #ここはmessageコントローラーだが、普通に他のテーブルのモデル、例えば上でいうgroupモデルも取ってくる事が出来る。
+    @message = Message.new
+  end
+
+  def create
+    Message.create(post_parms)
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
+
+  end
+
+  private
+
+  def post_parms
+        # params.require(:message)はモデルから送られてくるキーの名前。モデル名ではない。
+    params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: current_user.id)
   end
 
 

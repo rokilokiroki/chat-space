ac71279817b2af94641fef8cc3dc6923dea05869
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-05-10 15:53:47 +0900

    コメント箇所の修正

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
 
   factory :message do
     body              { Faker::Name.name }
-    user_id           "3"
-    group_id          "30"
+    user_id           { Faker::Number.between(1, 10) }
+    group_id          { Faker::Number.between(14, 38) }
     image ""
   end
 
